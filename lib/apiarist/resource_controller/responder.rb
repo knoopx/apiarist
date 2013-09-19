c29d9b99368a5b113802e3712f19ec9c03d555e2
Roger Campos <roger@itnig.net>
2013-09-19 22:43:10 +0200

    Delegate the render if the resource does not have to be serialized

--- lib/apiarist/resource_controller/responder.rb
+++ lib/apiarist/resource_controller/responder.rb
@@ -7,6 +7,8 @@ module Apiarist
       delegate :serialize, :resource_class, :_serialization_scope, to: :controller
 
       def display(resource, options = {})
+        return super(resource, options) if resource.is_a?(String)
+
         if resource.respond_to?(:each)
           root = resource_class.name.underscore.pluralize
         else
