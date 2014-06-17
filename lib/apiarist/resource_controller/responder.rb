0f25d95ac7bda3998332e4a2c60883742eda6c1c
Victor Martinez <knoopx@gmail.com>
2014-06-17 13:58:06 +0200

    FIX: resource_class is now a protected method

--- lib/apiarist/resource_controller/responder.rb
+++ lib/apiarist/resource_controller/responder.rb
@@ -4,13 +4,13 @@ require 'active_support/core_ext/module/delegation'
 module Apiarist
   module ResourceController
     class Responder < ActionController::Responder
-      delegate :serialize, :resource_class, :_serialization_scope, to: :controller
+      delegate :serialize, :_serialization_scope, to: :controller
 
       def display(resource, options = {})
         if resource.respond_to?(:each)
-          root = resource_class.name.underscore.pluralize
+          root = controller.send(:resource_class).name.underscore.pluralize
         else
-          root = resource_class.name.underscore
+          root = controller.send(:resource_class).name.underscore
         end
 
         super({root => serialize(resource, scope: _serialization_scope)}, options)
